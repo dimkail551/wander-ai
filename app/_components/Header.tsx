fea3a3e80564e52ebad89da47f3cc6c8bdee9515
dimkail551 <dimkail551@gmail.com>
2026-02-03 13:53:52 +0200

    Added Header component, Logo SVG and fixed Tailwind CSS config

---
+++ app/_components/Header.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Image from "next/image";
+import path from "path";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+const menuOptions = [
+  { 
+    name: "Home", 
+    path: "/" 
+},
+  { 
+    name: "Pricing", 
+    path: "/pricing" 
+},
+  { 
+    name: "Contact Us", 
+    path: "/contact-us" 
+},
+];
+
+function Header() {
+  return (
+    <div className='p-3 shadow-sm flex justify-between items-center '>
+         
+        <div className='flex gap-2 items-center'>
+            <Image src={'/logo.svg'} alt="Logo" width={160} height={40}/>
+            
+        </div>
+
+        
+        <div className='flex gap-8 items-center'>
+            {menuOptions.map((menu, index) => (
+                <Link href={menu.path} key={index}>
+                <h2 className='text-lg hover:scale-105 transition-all hover:text-primary'>{menu.name}</h2>
+                </Link>
+            ))}
+        </div>
+
+
+        <Button>Get Started</Button>
+    </div>
+  );
+}
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Header from "./_components/Header";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold">WanderAI </h1>
-      <p className="text-xl mt-4">Your AI Trip Planner is under construction.</p>
-    </main>
+    <div>
+        <Header />
+        
+    </div>
   );
 }
